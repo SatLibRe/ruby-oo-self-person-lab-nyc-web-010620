389e2ec91a57f6826c014e73c5c335ee7e19196b
Miles Higbie <miles.higbie@gmail.com>
2020-01-08 01:20:37 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,25 @@
 class Person 
+  
+  attr_accessor :name
+  attr_reader :bank_account,:happiness,:hygiene
+  
   def initialize(name)
     @name = name 
     @bank_account = 25
-    
+    @happiness = 8 
+    @hygiene = 8 
   end 
+  
+  def bank_account=(bank_account)
+    @bank_account = bank_account
+  end 
+  
+  def happiness=(happiness)
+    @happiness = happiness
+  end 
+  
+  def hygiene=(hygiene)
+    @hygiene = hygiene
+  end 
+  
 end 
